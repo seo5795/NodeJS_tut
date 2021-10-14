960b4da1342315e5d1f305ecbada420535c1c42d
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-15 00:02:32 +0900

    queryString에 따라서 다르게 동작하는 nodejs application 만들기

--- syntax/template.js
+++ syntax/template.js
@@ -5,10 +5,11 @@ var letter = 'Aute ' +name+ '\n\
 console.log(letter);
 
 var a = 1;
-
+var name2 = `정민`
 var letter = `Aute ${name}
 
-cupidatat amet elit ad ea occaecat pariatur elit ipsum occaecat consectetur. Amet ex aliquip excepteur occaecat ut magna duis dolor mollit sint enim. ${1+1} Do ullamco aute occaecat culpa magna. Esse magna est consectetur sunt pariatur ea sit ex eiusmod culpa. Aute Lorem eu ullamco sint velit exercitation.`;
+cupidatat amet elit ad ea occaecat pariatur elit ipsum occaecat consectetur. Amet ex aliquip excepteur occaecat ut magna duis dolor mollit sint enim. ${1+1} Do ullamco aute ${name2} occaecat culpa magna. Esse magna est consectetur sunt pariatur ea sit ex eiusmod culpa. Aute Lorem eu ullamco sint velit exercitation.`;
 
 console.log(letter);
+console.log(name2);
 //${} 변수를 치환해줌
