9dafabdf803720e4eb4d462bf89d37a75b4f8414
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-14 17:01:19 +0900

    template literal

---
+++ syntax/template.js
@@ -0,0 +1,14 @@
+var name = 'bun';
+var letter = 'Aute ' +name+ '\n\
+\ cupidatat amet elit ad ea occaecat pariatur elit ipsum occaecat consectetur. Amet ex aliquip excepteur occaecat ut magna duis dolor mollit sint enim. Do ullamco aute occaecat culpa magna. Esse magna est consectetur sunt pariatur ea sit ex eiusmod culpa. Aute Lorem eu ullamco sint velit exercitation.';
+
+console.log(letter);
+
+var a = 1;
+
+var letter = `Aute ${name}
+
+cupidatat amet elit ad ea occaecat pariatur elit ipsum occaecat consectetur. Amet ex aliquip excepteur occaecat ut magna duis dolor mollit sint enim. ${1+1} Do ullamco aute occaecat culpa magna. Esse magna est consectetur sunt pariatur ea sit ex eiusmod culpa. Aute Lorem eu ullamco sint velit exercitation.`;
+
+console.log(letter);
+//${} 변수를 치환해줌
