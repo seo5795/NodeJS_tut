88337f624cdff548f494e59d9e79dbfaff510166
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-18 02:16:37 +0900

    25. 홈페이지 구현

--- main.js
+++ main.js
@@ -12,8 +12,35 @@ var app = http.createServer(function(request,response){
     console.log(url.parse(_url, true).pathname);
     
     if(pathname === '/'){
-      // /?형식일때
+      // pathname === '/': /?형식일때
+      if(queryData.id === undefined){
+        fs.readFile(`data/${queryData.id}`, `utf8`, function(err,description){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var template = `<!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else{
       fs.readFile(`data/${queryData.id}`, `utf8`, function(err,description){
+        var title = queryData.id;
         var template = `<!doctype html>
       <html>
       <head>
@@ -36,8 +63,11 @@ var app = http.createServer(function(request,response){
       //a태그를 이용해 queryData에 값을 넣어주어 동적인 페이지 만들기
       response.writeHead(200);
       response.end(template);
+    
+      
    
   });
+}
     }else{
       // /?형식이 아닐때
       // ex)/favicon.ico
