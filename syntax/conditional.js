663b5ac39aefbf92a969316054ac33cae6b001fb
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-17 16:57:10 +0900

    args 개념의 이해와 조건문활용

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,6 +1,9 @@
+var args = process.argv;
+console.log(args[2]);
+//args배열의 0항에는 nodejs파일의 경로가, 1항에는 현재 파일의 경로가 들어가고 2항부터 입력값이 들어간다
 console.log('a');
 console.log('b');
-if(true){
+if(args[2] === '1'){
     console.log('c1');
 }
 else{
