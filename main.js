5b9ac2776f8d373fa30715f52008a3e33f294d6a
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-18 16:29:59 +0900

    34. function()을 이용하여 객체화 작업

--- main.js
+++ main.js
@@ -1,96 +1,85 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
- 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    //url.parse(_url, true): url을 분석하는 코드, 주어진 url정보를 분석해서 쉽게 쓸 수 있도록 해줌(추후 알려줄 예정)
-    var pathname = url.parse(_url, true).pathname;
-    
-    if(pathname === '/'){
-      // pathname === '/': /?형식일때
-      if(queryData.id === undefined){
-          fs.readdir('./data',function(error, filelist){
-            var title = 'Welcome';
-            var description = 'Hello, Node.js';
-            var list = `<ul>`;
 
-            var i = 0;
-            while(i < filelist.length){
-              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-              i = i + 1;
-            }
-            list = list + `</ul>`;
-            var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-          })
-         
-     
-    } else{
-      fs.readdir('./data',function(error, filelist){
-        var list = `<ul>`;
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+  var list = `<ul>`;
 
         var i = 0;
-        while(i < filelist.length){
+        while (i < filelist.length) {
           list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
           i = i + 1;
         }
         list = list + `</ul>`;
-      fs.readFile(`data/${queryData.id}`, `utf8`, function(err,description){
-        var title = queryData.id;
-        var template = `<!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-  
-      response.writeHead(200);
-      response.end(template);
-    
-      
-   
-  });
-});
+        return list;
 }
-    }else{
-      // /?형식이 아닐때
-      // ex)/favicon.ico
-      response.writeHead(404);
-      //200: 파일을 성공적으로 전송했다
-      response.end('Not found');
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  //url.parse(_url, true): url을 분석하는 코드, 주어진 url정보를 분석해서 쉽게 쓸 수 있도록 해줌(추후 알려줄 예정)
+  var pathname = url.parse(_url, true).pathname;
+
+  if (pathname === '/') {
+    // pathname === '/': /?형식일때
+    if (queryData.id === undefined) {
+      fs.readdir('./data', function (error, filelist) {
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `<h2>${title}</h2> 
+            <p>${description}</p>`);
+        response.writeHead(200);
+        response.end(template);
+      })
+
+
+    } else {
+      fs.readdir('./data', function (error, filelist) {
+       
+        fs.readFile(`data/${queryData.id}`, `utf8`, function (err, description) {
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2> 
+        <p>${description}</p>`);
+
+          response.writeHead(200);
+          response.end(template);
+
+
+
+        });
+      });
     }
-   
-    
-    
-    });
-    
-    
-    
+  } else {
+    // /?형식이 아닐때
+    // ex)/favicon.ico
+    response.writeHead(404);
+    //200: 파일을 성공적으로 전송했다
+    response.end('Not found');
+  }
+
+
+
+});
+
+
+
 app.listen(3000);
 
 
