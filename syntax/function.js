f69903dbea402474fad8831517c415ea0649b74f
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-18 14:03:48 +0900

    31~33. functino()

---
+++ syntax/function.js
@@ -0,0 +1,13 @@
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+console.log(Math.round(1.6));//반올림함수
+
+function sum(first, second){//parameter : 매개변수
+   return first+second;
+}
+
+console.log(sum(2,4)); //argument:인자
