ee854eba5dc4fde40f8e04654899eebd0243b8b0
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-18 13:04:32 +0900

    30. 글목록 출력하기-반복문, 배열을 사용

--- main.js
+++ main.js
@@ -7,38 +7,51 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     //url.parse(_url, true): url을 분석하는 코드, 주어진 url정보를 분석해서 쉽게 쓸 수 있도록 해줌(추후 알려줄 예정)
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
-    
-    console.log(url.parse(_url, true).pathname);
     
     if(pathname === '/'){
       // pathname === '/': /?형식일때
       if(queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, `utf8`, function(err,description){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var template = `<!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
+          fs.readdir('./data',function(error, filelist){
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
+            var list = `<ul>`;
+
+            var i = 0;
+            while(i < filelist.length){
+              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+              i = i + 1;
+            }
+            list = list + `</ul>`;
+            var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+          })
+         
+     
     } else{
+      fs.readdir('./data',function(error, filelist){
+        var list = `<ul>`;
+
+        var i = 0;
+        while(i < filelist.length){
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          i = i + 1;
+        }
+        list = list + `</ul>`;
       fs.readFile(`data/${queryData.id}`, `utf8`, function(err,description){
         var title = queryData.id;
         var template = `<!doctype html>
@@ -49,24 +62,20 @@ var app = http.createServer(function(request,response){
       </head>
       <body>
         <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
+        ${list}
         <h2>${title}</h2>
         <p>${description}</p>
       </body>
       </html>
       `;
-      //변수에다 한 페이지의 코드 전체를 넣어 실행시킴
-      //a태그를 이용해 queryData에 값을 넣어주어 동적인 페이지 만들기
+  
       response.writeHead(200);
       response.end(template);
     
       
    
   });
+});
 }
     }else{
       // /?형식이 아닐때
--- node/readdir.js
+++ node/readdir.js
@@ -4,3 +4,5 @@ var fs = require('fs');
 fs.readdir(testFolder, function(error, filelist){
     console.log(filelist);
 })
+//결과: [ 'CSS', 'HTML', 'JavaScript' ]
+//특정 디렉토리의 파일을 배열형태로 만들어 사용가능하게 해줌
