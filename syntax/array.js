92fef14066391295d74c248f5a9b82a7d20890cc
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-18 10:38:21 +0900

    28. 배열

---
+++ syntax/array.js
@@ -0,0 +1,13 @@
+var arr = ['a','b','c','d',true];
+console.log(arr[1]);
+console.log(arr[3]);
+
+//수정
+arr[2] = 3;
+console.log(arr);
+
+console.log(arr.length);
+
+//추가
+arr.push('e');
+console.log(arr);
