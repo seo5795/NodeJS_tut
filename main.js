75f6b88a21b7507559e437c3a3f499b02aba6578
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-19 12:58:06 +0900

    46. 글수정 완료

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href = "/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -45,7 +45,8 @@ var app = http.createServer(function (request, response) {
         var description = 'Hello, Node.js';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `<h2>${title}</h2> 
-            <p>${description}</p>`);
+            <p>${description}</p>`,
+            `<a href = "/create">create</a>`);
         response.writeHead(200);
         response.end(template);
       })
@@ -58,13 +59,11 @@ var app = http.createServer(function (request, response) {
           var title = queryData.id;
           var list = templateList(filelist);
           var template = templateHTML(title, list, `<h2>${title}</h2> 
-        <p>${description}</p>`);
+        <p>${description}</p>`,
+        `<a href = "/create">create</a> <a href= "/update?id=${title}">update</a>`);
 
           response.writeHead(200);
           response.end(template);
-
-
-
         });
       });
     }
@@ -74,13 +73,13 @@ var app = http.createServer(function (request, response) {
       var description = 'Hello, Node.js';
       var list = templateList(filelist);
       var template = templateHTML(title, list, `
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p><textarea name="description" placeholder="description"></textarea></p>
           <p><input type="submit"></p>
         </form>
 
-      `);
+      `, ``);
       response.writeHead(200);
       response.end(template);
     })
@@ -101,8 +100,48 @@ var app = http.createServer(function (request, response) {
           response.writeHead(302, {Location: `/?id=${title}`});//페이지를 REDIRECTION하라는 뜻:302
           response.end('success');
         })
-      })
-      
+      });
+    }else if(pathname === `/update`){
+      fs.readdir('./data', function (error, filelist) {
+       
+        fs.readFile(`data/${queryData.id}`, `utf8`, function (err, description) {
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `
+          <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p><textarea name="description" placeholder="description">${description}</textarea></p>
+          <p><input type="submit"></p>
+        </form>
+          `,
+        `<a href = "/create">create</a> <a href= "/update?id=${title}">update</a>`);
+
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){//데이터를 전송할 때 데이터를 한번에 처리하면 무리가 생기므로 조각조각의 정보를 수신할 때마다 서버가 콜백함수를 호출 하도록 약속 
+        body = body + data;
+      });
+      request.on('end', function(){
+        //정보수신이 끝났을 때 콜백함수 호출
+        //정보수신이 끝남
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        var id = post.id;
+        fs.rename(`data/${id}`, `data/${title}`, function(error){
+          fs.writeFile(`data/${title}`,description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});//페이지를 REDIRECTION하라는 뜻:302
+            response.end('success');
+          })
+        })
+        console.log(post);
+        
+      });
     }
    else {
     // /?형식이 아닐때
