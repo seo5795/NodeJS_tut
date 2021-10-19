39dd052b885dd20717043c4f6dd86267762baa2f
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-19 11:50:13 +0900

    글 생성 form + post방식 정보 받기 구현

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href = "/create">create</a>
     ${body}
   </body>
   </html>
@@ -66,7 +68,40 @@ var app = http.createServer(function (request, response) {
         });
       });
     }
-  } else {
+  }else if(pathname === '/create'){
+    fs.readdir('./data', function (error, filelist) {
+      var title = 'WEB - create';
+      var description = 'Hello, Node.js';
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p><textarea name="description" placeholder="description"></textarea></p>
+          <p><input type="submit"></p>
+        </form>
+
+      `);
+      response.writeHead(200);
+      response.end(template);
+    })
+
+  }
+    else if(pathname==='/create_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        //정보수신이 끝났을 때
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        console.log(post.title);
+      })
+      response.writeHead(200);
+      response.end('success');
+    }
+   else {
     // /?형식이 아닐때
     // ex)/favicon.ico
     response.writeHead(404);
