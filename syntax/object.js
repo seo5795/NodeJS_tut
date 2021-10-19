908ed4c051071aba1bace21fe05b7d1e92238c23
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-19 14:49:23 +0900

    객체와 배열 사용방식 차이

---
+++ syntax/object.js
@@ -0,0 +1,22 @@
+//배열 사용방식
+var member = ['egoing', 'k8805', '아리'];
+console.log(member[1]); //'k8805'
+var i =0;
+while(i<member.length){
+    console.log(member[i]);
+    i=i+1;
+}
+
+
+//객체 사용방식
+var roles = {
+    'programmer' : 'egoing',
+    'designer' : 'k8805',
+    'manager' : '아리'
+}
+console.log(roles.designer);//k8805
+console.log(roles['designer']);//k8805
+
+for(var name in roles){
+    console.log('object => ', name, 'value => ', roles[name]);
+}
