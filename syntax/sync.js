1055a47ca347bb4db4079aba6a67cc1ec4d3a450
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-22 11:57:36 +0900

    //노드js의 성능을 끌어올리기 위해서는 비동기적 방식을 채택하는 것이 좋다

--- syntax/sync.js
+++ syntax/sync.js
@@ -6,11 +6,15 @@ var fs = require('fs');
 // var result = fs.readFileSync('syntax/sample.txt', 'utf8');
 // console.log(result);
 // console.log('C');
+//readFileSync: 리턴값이 있음
 
 
 //readFile: 비동기적 동시에 처리
 console.log('A');
 fs.readFile('syntax/sample.txt', 'utf8', function(err, result){
     console.log(result);
+    //리턴값x
 });
 console.log('C');
+
+//노드js의 성능을 끌어올리기 위해서는 비동기적 방식을 채택하는 것이 좋다
